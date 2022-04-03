4607424f194c24eaa030228b1f6c9259afbdc4a6
anamerasul <anamerasul@gmail.com>
2022-04-04 04:57:21 +0600

    header logo color update

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,12 +7,11 @@ const Header = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
     return (
         <>
-            <nav className="flex flex-wrap items-center justify-between px-2 py-3 sticky top-0 bg-gray-800 text-white mb-3">
+            <nav className="flex flex-wrap items-center justify-between px-2 py-3 sticky top-0 bg-gray-800 shadow-lg  text-white mb-3">
                 <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
                     <div className="w-full relative flex justify-between md:w-auto md:static md:block md:justify-start">
                         <Link to='/'
-                            className="text-4xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-                            href="#pablo"
+                            className="text-4xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-fuchsia-300 md:text-white "
                         >
                             MAC-WORLD
                         </Link>

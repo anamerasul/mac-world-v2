0e72e6b201a028f0b65b70185cbe5d4a908c62ac
anamerasul <anamerasul@gmail.com>
2022-04-04 04:51:24 +0600

    responsive header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
         <>
             <nav className="flex flex-wrap items-center justify-between px-2 py-3 sticky top-0 bg-gray-800 text-white mb-3">
                 <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-                    <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                    <div className="w-full relative flex justify-between md:w-auto md:static md:block md:justify-start">
                         <Link to='/'
                             className="text-4xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
                             href="#pablo"
@@ -17,7 +17,7 @@ const Header = () => {
                             MAC-WORLD
                         </Link>
                         <button
-                            className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                            className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block md:hidden outline-none focus:outline-none"
                             type="button"
                             onClick={() => setNavbarOpen(!navbarOpen)}
                         >
@@ -26,12 +26,12 @@ const Header = () => {
                     </div>
                     <div
                         className={
-                            "lg:flex flex-grow items-center" +
+                            "md:flex flex-grow items-center" +
                             (navbarOpen ? " flex" : " hidden")
                         }
                         id="example-navbar-danger"
                     >
-                        <ul className="flex flex-col lg:flex-row list-none  lg:ml-auto">
+                        <ul className="flex flex-col md:flex-row list-none  md:ml-auto">
                             <li className="nav-item">
                                 <CustomLink className="px-3 py-2 flex items-center uppercase" to='/'>HOME</CustomLink>
                             </li>

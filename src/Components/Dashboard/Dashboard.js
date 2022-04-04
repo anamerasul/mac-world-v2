c432424eaff7e7b2ceccfc45868ad9bcd0d98720
anamerasul <anamerasul@gmail.com>
2022-04-05 05:06:36 +0600

    spinner update

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import UseChartHook from '../Hooks/UseChartHook';
 import LineChartDiv from '../LineChartDiv/LineChartDiv';
 import SimpleAreaChartDiv from '../SimpleAreaChartDiv/SimpleAreaChartDiv';
 import SimpleBarChartDiv from '../SimpleBarChartDiv/SimpleBarChartDiv';
+import Spinner from '../Spinner/Spinner';
 import TwoLevelPieChartDiv from '../TwoLevelPieChartDiv/TwoLevelPieChartDiv';
 
 
@@ -14,21 +15,32 @@ const Dashboard = () => {
 
     const [chart] = UseChartHook([])
     return (
-        <div>
 
+        <div className=" grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
+            {
+                chart.length === 0 ? (<Spinner></Spinner>) : (<LineChartDiv chart={chart}></LineChartDiv>)
 
-            <div className=" grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
-                <LineChartDiv chart={chart}></LineChartDiv>
-                <SimpleAreaChartDiv chart={chart}></SimpleAreaChartDiv>
-                <SimpleBarChartDiv chart={chart}></SimpleBarChartDiv>
-                <TwoLevelPieChartDiv chart={chart}></TwoLevelPieChartDiv>
+            }
 
+            {
+                chart.length === 0 ? (<Spinner></Spinner>) : (<SimpleAreaChartDiv chart={chart}></SimpleAreaChartDiv>)
 
-            </div>
+            }
+            {
+                chart.length === 0 ? (<Spinner></Spinner>) : (<SimpleBarChartDiv chart={chart}></SimpleBarChartDiv>)
 
+            }
+
+            {
+                chart.length === 0 ? (<Spinner></Spinner>) : (<TwoLevelPieChartDiv chart={chart}></TwoLevelPieChartDiv>)
+
+            }
 
         </div>
+
+
     );
 };
 
 export default Dashboard;
+

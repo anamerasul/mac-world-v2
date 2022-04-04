876c35a36775bdceed9bae2f7cb9f8a07d4617ec
anamerasul <anamerasul@gmail.com>
2022-04-05 02:37:34 +0600

    about us component add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
 import Blogs from './Components/Blogs/Blogs';
 import Dashboard from './Components/Dashboard/Dashboard';
 import Header from './Components/Header/Header';
@@ -16,6 +17,7 @@ function App() {
         <Route path='/reviews' element={<Review></Review>}></Route>
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
 
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>

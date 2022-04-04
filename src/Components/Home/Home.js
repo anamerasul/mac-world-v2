6bced3b3be6e0d21231fc1da695274400ea6ab5b
anamerasul <anamerasul@gmail.com>
2022-04-05 03:14:22 +0600

    home comopent update

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.css'
 import laptop from '../../laptop.png'
 import HomeReview from '../HomeReview/HomeReview';
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
@@ -10,9 +11,9 @@ const Home = () => {
             <div className="home-main-container">
                 <div className='title pl-6 max-w-[100%] order-2  md:order-1'>
                     <h1 className="text-6xl uppercase font-black "> welcome to <span className="hover:text-[#09f2b1]">mac</span>  world </h1>
-                    <p className='text-3xl font-normal px-2 mt-8 mb-12 lg:mr-32'>Macworld is your ultimate guide to Apple's product universe, explaining what's new, what's best and how to make the most out of the products you love.</p>
+                    <p className='text-3xl font-normal px-2 mt-8 mb-12 lg:mr-32'>Mac world is your ultimate guide to Apple's product universe, explaining what's new, what's best and how to make the most out of the products you love.</p>
 
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">Next</button>
+                    <Link to='/dashboard' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">Next</Link>
 
                 </div>
 

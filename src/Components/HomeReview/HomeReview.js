d2ad717c6ec6e7a0901ba64488a2675962a576a9
anamerasul <anamerasul@gmail.com>
2022-04-05 01:33:41 +0600

    homeReview update

--- src/Components/HomeReview/HomeReview.js
+++ src/Components/HomeReview/HomeReview.js
@@ -7,7 +7,8 @@ const HomeReview = () => {
     const [reviews, setReviews] = UseLaptopReviewHook([])
     return (
         <div className="my-10">
-            <h2 className='text-center text-2xl font-bold mb-10'>What client says about our products</h2>
+            <h2 className='text-center text-2xl font-bold mb-10 uppercase'>Our client review ({reviews.slice(0, 3).length})</h2>
+
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4  md:gap-x-4 md:gap-y-8">
                 {
--- src/Components/TwoLevelPieChartDiv/TwoLevelPieChartDiv.js
+++ src/Components/TwoLevelPieChartDiv/TwoLevelPieChartDiv.js
@@ -10,25 +10,6 @@ const TwoLevelPieChartDiv = ({ chart }) => {
             <h1 className="text-2xl ml-4 uppercase"> revenue vs investment</h1>
 
             <PieChart className="mt-8" width={320} height={400}>
-                {/* <Pie
-                    data={chart}
-                    dataKey="investment"
-                    cx={200}
-                    cy={200}
-                    outerRadius={60}
-                    fill="#8884d8"
-                />
-                <Pie
-                    data={chart}
-                    dataKey="revenue"
-                    cx={200}
-                    cy={200}
-                    innerRadius={70}
-                    outerRadius={90}
-                    fill="#82ca9d"
-                    label */}
-                {/* /> */}
-
                 <Pie data={chart} dataKey="investment" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
                 <Pie data={chart} dataKey="revenue" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
             </PieChart>
